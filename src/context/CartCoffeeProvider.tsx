d0f6f62a4164c7b5528b8acd8bdc96de7d01630d
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-12-10 18:15:23 -0300

    Create function o reducer

--- src/context/CartCoffeeProvider.tsx
+++ src/context/CartCoffeeProvider.tsx
@@ -1,7 +1,8 @@
 import { createContext, ReactNode, useReducer, useState } from 'react';
 import coffeeList from '../coffee.seed.json';
+import { CartStateReducer } from '../reducers/reducer';
 
-interface Coffee {
+export interface Coffee {
     id: string;
     title: string;
     subtitle: string;
@@ -10,7 +11,7 @@ interface Coffee {
     imageUrl: string;
 }
 
-interface selectedCoffee {
+export interface selectedCoffee {
     coffee: Coffee;
     quantity: number;
 }
@@ -34,40 +35,7 @@ export const CartContext = createContext({} as ICartContext);
 export function CartCoffeeContextProvider({ children }: CartCoffeeContextProviderProps) {
     const coffees: Coffee[] = coffeeList.coffees;
 
-    const [cartState, dispatch] = useReducer((state: selectedCoffee[], action: any) => {
-        if (action.type == 'ADD_NEW_COFFEE') {
-            const selectedCoffee: selectedCoffee = Object.assign({
-                coffee: getSelectedCoffeeById(action.payload.coffeeId),
-                quantity: action.payload.quantity
-            });
-            return [...state, selectedCoffee];
-        }
-
-        if (action.type === 'REMOVE_COFFEE') {
-            const newState = state.filter(item => {
-                return item.coffee.id !== action.payload.coffeeId;
-            });
-
-            return newState;
-        }
-
-        if (action.type === 'CHANGE_QUANTITY') {
-            return state.map(item => {
-                if (item.coffee.id === action.payload.coffeeId) {
-                    const updateCoffee: selectedCoffee = Object.assign({
-                        coffee: getSelectedCoffeeById(action.payload.coffeeId),
-                        quantity: action.payload.quantity
-                    });
-                    console.log(updateCoffee)
-                    return updateCoffee;
-                } else {
-                    return item;
-                }
-            });
-        }
-
-        return state;
-    }, []);
+    const [cartState, dispatch] = useReducer(CartStateReducer, []);
 
     const [selectedCoffees, setSelectedCoffees] = useState<selectedCoffee[]>([]);
     // const [quantity, setQuantity] = useState(0);
@@ -78,7 +46,7 @@ export function CartCoffeeContextProvider({ children }: CartCoffeeContextProvide
         dispatch({
             type: 'ADD_NEW_COFFEE',
             payload: {
-                coffeeId,
+                coffee: getSelectedCoffeeById(coffeeId),
                 quantity
             }
         });
@@ -88,7 +56,7 @@ export function CartCoffeeContextProvider({ children }: CartCoffeeContextProvide
         dispatch({
             type: 'REMOVE_COFFEE',
             payload: {
-                coffeeId: coffeeIdToRemove
+                coffee: getSelectedCoffeeById(coffeeIdToRemove),
             }
         });
     }
@@ -97,7 +65,7 @@ export function CartCoffeeContextProvider({ children }: CartCoffeeContextProvide
         dispatch({
             type: 'CHANGE_QUANTITY',
             payload: {
-                coffeeId,
+                coffee: getSelectedCoffeeById(coffeeId),
                 quantity
             }
         });
--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,19 +1,36 @@
+import { selectedCoffee } from "../context/CartCoffeeProvider";
 
+export function CartStateReducer(state: selectedCoffee[], action: any) {
+    if (action.type == 'ADD_NEW_COFFEE') {
+        const selectedCoffee: selectedCoffee = Object.assign({
+            coffee: action.payload.coffee,
+            quantity: action.payload.quantity
+        });
+        return [...state, selectedCoffee];
+    }
 
-interface Coffee {
-    id: string;
-    title: string;
-    subtitle: string;
-    tags: string[];
-    price: number;
-    imageUrl: string;
-}
+    if (action.type === 'REMOVE_COFFEE') {
+        const newState = state.filter(item => {
+            return item.coffee.id !== action.payload.coffee.id;
+        });
 
-interface ICartItem {
-    coffees: Coffee[];
-    quantity: number;
-}
+        return newState;
+    }
 
-export function CartStateReducer() {
+    if (action.type === 'CHANGE_QUANTITY') {
+        return state.map(item => {
+            if (item.coffee.id === action.payload.coffee.id) {
+                const updateCoffee: selectedCoffee = Object.assign({
+                    coffee: action.payload.coffee,
+                    quantity: action.payload.quantity
+                });
+                console.log(updateCoffee)
+                return updateCoffee;
+            } else {
+                return item;
+            }
+        });
+    }
 
+    return state;
 }
