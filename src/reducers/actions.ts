99c4ffe50d1937a31688a72669db358090fda3cc
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2022-12-11 09:56:11 -0300

    Create actions file

---
+++ src/reducers/actions.ts
@@ -0,0 +1,44 @@
+import { Coffee } from "../context/CartCoffeeProvider";
+
+export enum ActionTypes {
+    // eslint-disable-next-line no-unused-vars
+    ADD_NEW_COFFEE = 'ADD_NEW_COFFEE',
+    // eslint-disable-next-line no-unused-vars
+    REMOVE_COFFEE = 'REMOVE_COFFEE',
+    // eslint-disable-next-line no-unused-vars
+    CHANGE_QUANTITY = 'CHANGE_QUANTITY',
+}
+
+export type ActionTypesProps =
+    | { type: ActionTypes.ADD_NEW_COFFEE; payload: { coffee: Coffee, quantity: number } }
+    | { type: ActionTypes.REMOVE_COFFEE; payload: { coffeeId: string } }
+    | { type: ActionTypes.CHANGE_QUANTITY; payload: { coffee: Coffee, quantity: number } }
+
+export function addNewCoffeeAction(coffee: Coffee, quantity: number): ActionTypesProps {
+    return {
+        type: ActionTypes.ADD_NEW_COFFEE,
+        payload: {
+            coffee,
+            quantity
+        }
+    }
+}
+
+export function removeCoffeeAction(coffeeId: string): ActionTypesProps {
+    return {
+        type: ActionTypes.REMOVE_COFFEE,
+        payload: {
+            coffeeId
+        }
+    }
+}
+
+export function changeCoffeeQuantityAction(coffee: Coffee, quantity: number): ActionTypesProps {
+    return {
+        type: ActionTypes.CHANGE_QUANTITY,
+        payload: {
+            coffee,
+            quantity
+        }
+    }
+}
